9d6a21e20a9d50acda6b772242e2041f38757deb
UGiOS (Hex) <126173453+UGiOS@users.noreply.github.com>
2024-06-15 22:20:09 +0400

    UpdateLog.lua

---
+++ UpdateLog.lua
@@ -0,0 +1,27 @@
+--[[ UpdateLog №1.0.0
+
+# Additions
+
+[+] Working Script Hub Loader;
+  [+] Build A Boat For Treasure Added As A Game;
+    [+] Bug Fixes;
+      [+] Obfuscated Scripts;
+        [+] New ScreenGUI.
+
+# Removals
+
+-- SPACE IN BETWEEN
+
+[-] Non-Obfuscated Scripts;
+  [-] Old ScreenGUI Removed. ]]
+
+--[[ UpdateLog №0.5.0 (BETA)
+
+# Additions
+
+[+] New ScreenGUI;
+  [+] Semi-Working Script Hub Loader.
+
+# Removals
+
+[-] Mobile Support. (Will Come Later On) ]]
