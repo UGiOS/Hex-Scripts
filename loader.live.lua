2f4a6c3f23591ccfc90d919c813450786e29b1d6
UGiOS (Hex) <126173453+UGiOS@users.noreply.github.com>
2024-06-15 21:55:21 +0400

    Update and rename loader.live to loader.live.lua

---
+++ loader.live.lua
@@ -0,0 +1,13 @@
+print("Hex Hub loader.live is loading...")
+
+local correctPlaceID = "537413528" -- Replace this with your actual Place ID
+
+local function runSpecificScript()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/UGiOS/Hex-Scripts/main/BuildABoatForTreasure.lua'))()
+end
+
+if tostring(game.PlaceId) == correctPlaceID then
+    runSpecificScript()
+else
+    print("loader.live.lua Unavailable!")
+end
