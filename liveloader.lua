2a4d36a7f08c126512a6791e07efeaa6cc5584e8
UGiOS (Hex) <126173453+UGiOS@users.noreply.github.com>
2024-06-15 21:36:08 +0400

    Update liveloader.lua

--- liveloader.lua
+++ liveloader.lua
@@ -1,2 +1,4 @@
---[[ print("Hex Scripts loader.lua loaded...")
-  ]]
+if game.PlaceId == 537413528 then
+loadstring(game:HttpGet('https://raw.githubusercontent.com/UGiOS/Hex-Scripts/main/BABFT.lua'))()
+  
+print("Hex Scripts loader.lua loaded..."
