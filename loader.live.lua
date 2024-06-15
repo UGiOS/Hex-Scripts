b98e52e6e2c9eff13568c98d89f55837eb2accf9
UGiOS (Hex) <126173453+UGiOS@users.noreply.github.com>
2024-06-15 22:27:09 +0400

    new again

--- loader.live.lua
+++ loader.live.lua
@@ -1,6 +1,6 @@
 print("Hex Hub loader.live.lua is loading...")
 
-local correctPlaceID = "537413528" -- Replace this with your actual Place ID
+local correctPlaceID = "537413528" -- Replace in Brackets with your own PlaceID
 
 local function runSpecificScript()
     loadstring(game:HttpGet('https://raw.githubusercontent.com/UGiOS/Hex-Scripts/main/BuildABoatForTreasure.lua'))()
