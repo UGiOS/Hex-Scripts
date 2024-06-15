62f059b6b6d9d6a330a486a58927d911808d4e83
UGiOS (Hex) <126173453+UGiOS@users.noreply.github.com>
2024-06-15 21:32:42 +0400

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,6 +1,2 @@
-print("Hex Scripts loader.lua loaded...")
-
-if game.PlaceId == 537413528 then
-loadstring(game:HttpGet('https://raw.githubusercontent.com/UGiOS/Hex-Scripts/main/BABFT.lua'))()
-
--- elseif game.PlaceId == (NUMBERS) then
+--[[ print("Hex Scripts loader.lua loaded...")
+  ]]
